9f78fa81e064277770854df74dc9e625446f948d
rapagelanus <gedasss+github@gmail.com>
2013-02-11 20:40:32 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandomojiApp::Application.config.secret_token = '3865d98e235155238b65ae0de2f51886aff3ec0fe4301c9c56c9a86f35045f0e460febcc220a40cbd3bd128639e67faa54979611635103b794a34cfda3794924'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BandomojiApp::Application.config.session_store :cookie_store, key: '_bandomoji_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BandomojiApp::Application.config.session_store :active_record_store
