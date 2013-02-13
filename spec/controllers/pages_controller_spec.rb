9e2f39055645f83a1a8708b324212a56feffc596
rapagelanus <gedasss+github@gmail.com>
2013-02-13 23:03:08 +0200

    Pridetas site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  render.views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                            :content => "Ruby on rails bandomoji | Home")
+    end
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,6 +24,20 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => "Ruby on rails bandomoji | Contact")
   end
 
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => "Ruby on rails bandomoji | About")
+  end
 end
