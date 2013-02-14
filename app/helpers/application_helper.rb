b0cd9a531b62f150c968e808eac41021e6437292
rapagelanus <gedasss+github@gmail.com>
2013-02-14 18:23:28 +0200

    title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #helperis title
+  def title
+  base_title = "Ruby on rails bandomoji programa"
+  if @title.nil?
+    base_title
+  else
+    "#{base_title} | #{@title}"
+  end
+  end
 end
