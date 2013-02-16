bbbbe0fb4577a76a8485067f24dcc35b9611d1d0
rapagelanus <gedasss+github@gmail.com>
2013-02-16 16:45:43 +0200

    istaisytos klaidos

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
-  render.views
+  render_views
 
   before(:each) do
-    base_title = "Ruby on rails bandomoji"
+    @base_title = "Ruby on rails bandomoji"
   end
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
@@ -28,10 +29,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
                                     :content => "#{@base_title} | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -43,5 +46,6 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                                     :content => "#{@base_title} | About")
+    end
   end
 end
