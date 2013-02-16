63b795e4e2e30018e4042209a3226c420f36a5a1
rapagelanus <gedasss+github@gmail.com>
2013-02-16 16:14:23 +0200

    stylesheets

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render.views
 
+  before(:each) do
+    base_title = "Ruby on rails bandomoji"
+  end
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -11,7 +15,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                            :content => "Ruby on rails bandomoji | Home")
+                                    :content => "#{@base_title} | Home")
     end
     it "should have a non-blank body" do
       get 'home'
@@ -27,7 +31,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                                    :content => "Ruby on rails bandomoji | Contact")
+                                    :content => "#{@base_title} | Contact")
   end
 
   describe "GET 'about'" do
@@ -38,6 +42,6 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                                    :content => "Ruby on rails bandomoji | About")
+                                    :content => "#{@base_title} | About")
   end
 end
